30eb5319d90174b96acfcd36f72a01ac1bed57bf
Travis Vachon <travis@copious.com>
2012-07-11 13:34:59 -0400

    preparing for 2.2.3 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject resque-clojure "0.2.2"
+(defproject utahstreetlabs/resque-clojure "0.2.3"
   :description "Redis based library for asynchronous processing"
   :url "https://github.com/jxa/resque-clojure"
   :dependencies [[org.clojure/data.json "0.1.2"]
